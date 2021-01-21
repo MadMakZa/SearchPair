9a98d3e1382062a6843eb0c0172094189f4ff532
MadMakZa <immakza@gmail.com>
2021-01-22 00:58:55 +0300

    array list with resourses

--- app/src/main/java/com/example/searchpair/MainActivity.java
+++ app/src/main/java/com/example/searchpair/MainActivity.java
@@ -8,13 +8,19 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     int[] array = new int[16];
+    List<ImageView> listImageView = new ArrayList<>();
     Map<Integer, ImageView> map1 = new HashMap<>();
 
     private ImageView image1, image2, image3, image4,
@@ -30,16 +36,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
-        generateRandom();
-        print();
         imagesOnClickListener();
 
 
     }
     //инициализация
     private void init(){
-        //в начале присвоить айдишники
+        //анимация
         animation1 = AnimationUtils.loadAnimation(this, R.anim.anim_one);
+        //айдишники картинок
         image1 = findViewById(R.id.imageView1);
         image2 = findViewById(R.id.imageView2);
         image3 = findViewById(R.id.imageView3);
@@ -52,33 +57,64 @@ public class MainActivity extends AppCompatActivity {
         image10 = findViewById(R.id.imageView10);
         image11 = findViewById(R.id.imageView11);
         image12 = findViewById(R.id.imageView12);
-        map1.put(1, image1);
-        map1.put(2, image2);
-        map1.put(3, image3);
-        map1.put(4, image4);
-        map1.put(5, image5);
-        map1.put(6, image6);
-        map1.put(7, image7);
-        map1.put(8, image8);
-        map1.put(9, image9);
-        map1.put(10, image10);
-        map1.put(11, image11);
-        map1.put(12, image12);
+        //присвоить ресурсы вьюшкам
+        image1.setImageResource(R.drawable.token5);
+        image2.setImageResource(R.drawable.token5d);
+        image3.setImageResource(R.drawable.token10);
+        image4.setImageResource(R.drawable.token10d);
+        image5.setImageResource(R.drawable.token25);
+        image6.setImageResource(R.drawable.token25d);
+        image7.setImageResource(R.drawable.token50);
+        image8.setImageResource(R.drawable.token50d);
+        image9.setImageResource(R.drawable.token100);
+        image10.setImageResource(R.drawable.token100d);
+        image11.setImageResource(R.drawable.token250);
+        image12.setImageResource(R.drawable.token250d);
+        //заполнение аррайлиста
+        listImageView.add(image1);
+        listImageView.add(image2);
+        listImageView.add(image3);
+        listImageView.add(image4);
+        listImageView.add(image5);
+        listImageView.add(image6);
+        listImageView.add(image7);
+        listImageView.add(image8);
+        listImageView.add(image9);
+        listImageView.add(image10);
+        listImageView.add(image11);
+        listImageView.add(image12);
+        //перемешать
+        Collections.shuffle(listImageView);
+        //добавить картинки в мапу
+        map1.put(1, listImageView.get(0));
+        map1.put(2, listImageView.get(1));
+        map1.put(3, listImageView.get(2));
+        map1.put(4, listImageView.get(3));
+        map1.put(5, listImageView.get(4));
+        map1.put(6, listImageView.get(5));
+        map1.put(7, listImageView.get(6));
+        map1.put(8, listImageView.get(7));
+        map1.put(9, listImageView.get(8));
+        map1.put(10, listImageView.get(9));
+        map1.put(11, listImageView.get(10));
+        map1.put(12, listImageView.get(11));
+
+
+
+
 
 
     }
-    //рандом
-    private void generateRandom(){
-        for (int i = 0; i < array.length; i++){
-            array[i] = 1 + (int)(Math.random()*16);
-        }
+    //random res
+    private void randomRes() {
+        int i = (int) (3 * Math.random() + 1);
+        String RandomS = "token" + i;
+        int resID = getResources().getIdentifier(RandomS, "drawable", getApplicationContext().getPackageName());
+        image12.setImageResource(resID);
     }
-    //принт логов
-    private void print(){
-        for (int i = 0; i < array.length; i++){
-            Log.d("Log", "" +array[i]);
-        }
-    }
+
+
+
     //установить слушатель нажатий для всех картинок
     public void imagesOnClickListener(){
         // Получаем набор элементов
@@ -90,12 +126,11 @@ public class MainActivity extends AppCompatActivity {
             pair.getValue().setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    map1.get(pair.getKey()).setImageResource(R.drawable.ic_launcher_foreground);
                     map1.get(pair.getKey()).startAnimation(animation1);
                     animation1.setAnimationListener(new Animation.AnimationListener() {
                         @Override
                         public void onAnimationStart(Animation animation) {
-                            map1.get(pair.getKey()).setImageResource(R.drawable.ic_launcher_background);
+//                            map1.get(pair.getKey()).setImageResource(R.drawable.);
                         }
 
                         @Override
@@ -113,24 +148,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-    private void animationListener(){
-        animation1.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-                image1.setImageResource(R.drawable.ic_launcher_background);
-            }
 
-            @Override
-            public void onAnimationEnd(Animation animation) {
 
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
-    }
 
 
 }
