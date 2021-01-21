eee4606fc45f912f486d502938d9d64a81a777fd
MadMakZa <immakza@gmail.com>
2021-01-21 22:25:15 +0300

    set resource work

--- app/src/main/java/com/example/searchpair/MainActivity.java
+++ app/src/main/java/com/example/searchpair/MainActivity.java
@@ -5,49 +5,95 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import java.util.Iterator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     int[] array = new int[16];
+    Map<Integer, ImageView> map1 = new HashMap<>();
 
-    private ImageView image;
+    private ImageView image1, image2, image3, image4;
+    Animation animation1 = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //в начале присвоить айдишник
-        image = findViewById(R.id.imageView1);
 
+        init();
         generateRandom();
         print();
         clickImage();
+        imagesOnClickListener();
 
 
     }
     //инициализация
     private void init(){
+        //в начале присвоить айдишники
+        animation1 = AnimationUtils.loadAnimation(this, R.anim.anim_one);
+        image1 = findViewById(R.id.imageView1);
+        image2 = findViewById(R.id.imageView2);
+        image3 = findViewById(R.id.imageView3);
+        image4 = findViewById(R.id.imageView4);
+        map1.put(1, image1);
+        map1.put(2, image2);
+        map1.put(3, image3);
+        map1.put(4, image4);
+
 
     }
-
+    //рандом
     private void generateRandom(){
         for (int i = 0; i < array.length; i++){
             array[i] = 1 + (int)(Math.random()*16);
         }
     }
-
+    //принт логов
     private void print(){
         for (int i = 0; i < array.length; i++){
             Log.d("Log", "" +array[i]);
         }
     }
+    //установить слушатель нажатий для всех картинок
+    public void imagesOnClickListener(){
+        // Получаем набор элементов
+        Set<Map.Entry<Integer, ImageView>> set = map1.entrySet();
+        //Отобразить набор
+        for (final Map.Entry<Integer, ImageView> pair : set) {
+            Log.d("Map",(pair.getKey() + ": "));
+            Log.d("Map",(pair.getValue() + ": "));
+            pair.getValue().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    map1.get(pair.getKey()).setImageResource(R.drawable.ic_launcher_foreground);
+                }
+            });
+        }
+        // Добавляем значение
+//        map1.get(1).setImageResource(R.drawable.ic_launcher_background);
+//        map1.get(1).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //запустить анимацию
+//                map1.get(1).setImageResource(R.drawable.ic_launcher_foreground);
+//                //запустить метод
+//            }
+//        });
+    }
+
     public void clickImage(){
-        image.setOnClickListener(new View.OnClickListener() {
+        image1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                image.setImageResource(R.drawable.ic_launcher_background);
+
             }
         });
     }
