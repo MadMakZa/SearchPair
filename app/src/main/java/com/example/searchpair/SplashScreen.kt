74c3aa93ff85d12502e13712f2b1e87967b29cae
MadMakZa <immakza@gmail.com>
2021-03-20 13:10:30 +0300

    add splash screen window

---
+++ app/src/main/java/com/example/searchpair/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.example.searchpair
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+/**
+ * Загрузочный экран
+ */
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+        val intent = Intent(this, GameMainActivity::class.java)
+        startActivity(intent)
+//        overridePendingTransition(R.anim.open_activity, R.anim.close_activity)
+        finish()
+        }, 3000)
+    }
+}
